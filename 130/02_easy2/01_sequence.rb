336ec414a6c44e2db19f7782032817d82414c3a8
Will Lotherington <dev@kaplop.com>
2018-09-09 21:30:55 -0500

    add exercise files

--- 130/02_easy2/01_sequence.rb
+++ 130/02_easy2/01_sequence.rb
@@ -1,1 +1,10 @@
-01_sequence.rb
+def step(start, stop, step)
+  idx = start
+
+  while idx <= stop
+    yield(idx)
+    idx += step
+  end
+end
+
+step(1, 10, 3) { |value| puts "value = #{value}" }
---
+++ 130/02_easy2/03_map.rb
@@ -0,0 +1,8 @@
+def map(arr)
+end
+
+map([1, 3, 6]) { |value| value**2 } == [1, 9, 36]
+map([]) { |value| true } == []
+map(['a', 'b', 'c', 'd']) { |value| false } == [false, false, false, false]
+map(['a', 'b', 'c', 'd']) { |value| value.upcase } == ['A', 'B', 'C', 'D']
+map([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]]
