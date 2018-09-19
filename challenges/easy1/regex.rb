1fa97330caaefa32d4197d2383d5b8828e15e6b8
Will Lotherington <dev@kaplop.com>
2018-09-19 18:33:19 -0500

    add challenge files

--- challenges/easy1/regex.rb
+++ challenges/easy1/regex.rb
@@ -60,12 +60,12 @@
 #   date.split(/\-/).reverse.join('.')
 # end
 
-def format_date(date)
-  date.sub(/(\d\d\d\d)[\-\/](\d\d)[\-\/](\d\d)/, '\3.\2.\1')
-end
+# def format_date(date)
+#   date.sub(/(\d\d\d\d)[\-\/](\d\d)[\-\/](\d\d)/, '\3.\2.\1')
+# end
 
-p format_date('2016-06-17') # -> '17.06.2016'
-p format_date('2016/06/17') # -> '2016/06/17' (no change)
-p format_date('2016-06-17') # -> '17.06.2016'
-p format_date('2017/05/03') # -> '03.05.2017'
-p format_date('2015/01-31') # -> '2015/01-31' (no change)
+# p format_date('2016-06-17') # -> '17.06.2016'
+# p format_date('2016/06/17') # -> '2016/06/17' (no change)
+# p format_date('2016-06-17') # -> '17.06.2016'
+# p format_date('2017/05/03') # -> '03.05.2017'
+# p format_date('2015/01-31') # -> '2015/01-31' (no change)
